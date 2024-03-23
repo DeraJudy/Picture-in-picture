91be93bfad13321fd7477a10c3e8903a6d6bd26a
DeraJudy <110675235+DeraJudy@users.noreply.github.com>
2024-03-22 21:20:12 -0700

    javacript commit

--- script.js
+++ script.js
@@ -4,9 +4,24 @@ const button = document.getElementById('button');
 // Prompt to select media stream, pass to video element then play
 async function selectMediaStream() {
     try {
-        // 
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
     } catch (error) {
         // Catch error here
-        
     }
 }
+
+button.addEventListener('click', async () => {
+    // disable button
+    button.disabled = true;
+    // start picture in picture
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disabled = false;
+})
+
+// on load
+selectMediaStream();
